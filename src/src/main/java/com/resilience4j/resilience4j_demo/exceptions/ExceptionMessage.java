3272cb4b40ed49f2a17976434a47f874893b43d7
vijayasai <vijaykesanupalli@gmail.com>
2025-02-20 15:45:13 -0500

    Delete src/src directory

--- src/src/main/java/com/resilience4j/resilience4j_demo/exceptions/ExceptionMessage.java
+++
@@ -1,9 +0,0 @@
-package com.resilience4j.resilience4j_demo.exceptions;
-
-public record ExceptionMessage(String timestamp,
-                               int status,
-                               String error,
-                               String message,
-                               String path) {
-}
-
